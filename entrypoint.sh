9f1656551826f8d8117b8b9f6a1542b9374449a8
rjdndjsjs9 <103624181+rjdndjsjs9@users.noreply.github.com>
2025-02-21 17:38:24 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 echo "Ensuring permissions..."
 mkdir -p /var/log/pterodactyl
-chown -R wings:wings /var/log/pterodactyl
+chown -R pterodactyl:pterodactyl /var/log/pterodactyl
 
-echo "Starting Wings with SSL..."
-exec /usr/local/bin/wings --config /etc/pterodactyl/config.yml
+echo "Starting Docker daemon..."
+service docker start
+
+echo "Starting Wings..."
+exec wings --config /etc/pterodactyl/config.yml
